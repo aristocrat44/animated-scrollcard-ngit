a13d24a70c7756114363c09cf7c6f5bd4f21a9bc
aristocrat44 <arcrt1993@gmail.com>
2024-08-08 08:34:42 +0545

    npm publish implemented

---
+++ src/components/DummyHeader.tsx
@@ -0,0 +1,9 @@
+const DummyHeader = () => {
+  return (
+    <div className="text-blue-700 cursor-pointer">
+      Hi, trying to publish to npm alu
+    </div>
+  );
+};
+
+export default DummyHeader;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "AnimatedScrollCardNGIT",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["es", "cjs"], // supports es and common js format
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "REACTDOM",
+        },
+      },
+    },
+    sourcemap: false,
+    emptyOutDir: true,
+  },
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: "./tsconfig.json",
+    }), // typeScript declaration files
+  ],
+});
