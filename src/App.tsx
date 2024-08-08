3160c2d19b32fdbb83c367ecc8413c84297311f0
Atish Maharjan <arcrt1993@gmail.com>
2024-08-08 08:42:52 +0545

    npm publish implemented (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { DummyHeader } from "animated-scrollcard-ngit";
+
 function App() {
   return (
     <>
       <h1 className="flex justify-center">Hello world</h1>
+      <DummyHeader />
     </>
   );
 }
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// Entry point for components
+
+import DummyHeader from "@/components/DummyHeader";
+
+export { DummyHeader };
